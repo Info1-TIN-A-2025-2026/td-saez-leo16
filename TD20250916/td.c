b330c34008f47428f700e6f38ca34ed0e9d3bcb4
saez-leo16 <leonel.saez@heig-vd.ch>
2025-09-16 12:32:29 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23;
+	signed int account_value = -42; // signed optional
+	float temperature = 18.1;
+	double pi = 3.141592653589793; // 64 bit
+}
