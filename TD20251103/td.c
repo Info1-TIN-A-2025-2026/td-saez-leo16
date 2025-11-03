50ee7226ecda8bc3f1f3507d62fd292b7664c6e3
saez-leo16 <leonel.saez@heig-vd.ch>
2025-11-03 13:29:03 +0000

    new TD

---
+++ TD20251103/td.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+	// Boucle While()
+	/*const unsigned int BASE = 10;
+
+	unsigned int n = atoi(argv[1]);
+	unsigned int w = n == 0 ? 1 : 0;
+
+	while(n > 0)
+	{
+		n /= BASE;
+		w++;
+	}
+
+	printf("w=%u\n", w);*/
+
+	unsigned int n = atoi(argv[1]);
+	unsigned int r = 0;
+
+	do
+	{
+		r *= 10;
+		r += n % 10;
+		n /= 10;
+
+	} while (n > 0);
+
+	printf("r = %u\n", r);
+
+	return 0;
+}
